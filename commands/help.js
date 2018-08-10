2c67009024e3df2d6a544bfc4552910d1398f62c
Unknown <oeps46@gmail.com>
2018-08-10 16:36:00 +0200

    updated help command

--- commands/help.js
+++ commands/help.js
@@ -4,5 +4,6 @@ exports.run = (client, message, args) => {
     message.channel.send(`These commands are available:` + `\n` +
     `${config.prefix}help: ${command.help}` + `\n` +
     `${config.prefix}reload: ${command.reload}` + `\n` +
-    `${config.prefix}prefix: ${command.prefix}`).catch(console.error);
+    `${config.prefix}prefix: ${command.prefix}` + `\n` +
+    `${config.prefix}purr: ${command.purr}`).catch(console.error);
 }
