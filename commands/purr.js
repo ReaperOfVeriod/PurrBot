bd1e632abbbbb3bfae6a9b2303846fc780f50ac5
Unknown <oeps46@gmail.com>
2018-08-10 16:00:08 +0200

    added purr command

---
+++ commands/purr.js
@@ -0,0 +1,16 @@
+exports.run = (client, message, args) => {
+    const path = "./images/";
+    const fs = require("fs");
+    
+    const nekoImage = fs.readdirSync(path)
+  
+    var neko = nekoImage[Math.floor(Math.random() * nekoImage.length)];
+    var randomNeko = path + neko;
+    message.channel.send({
+      files: [{
+        attachment: randomNeko,
+        name: "neko.gif"
+      }]
+    });
+  
+  }
