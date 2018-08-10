f21792c8f6b7c1c91a578d1aacfda576340e45c6
Unknown <oeps46@gmail.com>
2018-08-10 19:54:07 +0200

    rotating status update

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,11 @@
 const config = require("./../config.json");
 module.exports = (client, ready) => {
-    client.user.setActivity(config.game, {type: config.activityType} );
+    
+    var interval = setInterval(function () {
+        const watchArray = config.game;
+        var watch = watchArray[Math.floor(Math.random() * watchArray.length)];
+    
+
+        client.user.setActivity(watch, {type: config.activityType} );
+    }, 1 * 100000);
 };
